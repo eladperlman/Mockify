48c5bb6fc865ce7c2aa7676aede945bb1c066068
Elad Perlman <ep3213@gmail.com>
2023-07-02 17:37:41 -0400

    fixed empty form submission

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
 import { InputAdornment, OutlinedInput } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { playlistActions } from '../store';
 import { useDispatch } from 'react-redux';
 import { searchSongs } from '../global/constants';
@@ -15,35 +15,41 @@ const SearchBar = ({ placeholder, children }: Props) => {
   const [search, setSearch] = useState<string>('');
   const dispatch = useDispatch();
   const { data } = useSearchTracksQuery(search, {
-    skip: search === ''
+    skip: search === '',
   });
 
   return (
     <form
       className="flex justify-center min-w-[50%]"
       onSubmit={(e) => {
-        console.log("hello")
         e.preventDefault();
-        dispatch(
-          playlistActions.setSearchResults(
-            data.map((track: any) => {
-              return {
-                title: track.name,
-                length: track.duration_ms,
-                img: track.album.images[2].url,
-                artist: track.artists[0].name,
-                id: track.id,
-                album: track.album.name,
-                bigImg: track.album.images[1].url,
-              };
-            })
-          )
-        );
-        dispatch(playlistActions.setCurPlaylist(searchSongs));
+        if (data) {
+          dispatch(
+            playlistActions.setSearchResults(
+              data.map((track: any) => {
+                return {
+                  title: track.name,
+                  length: track.duration_ms,
+                  img: track.album.images[2].url,
+                  artist: track.artists[0].name,
+                  id: track.id,
+                  album: track.album.name,
+                  bigImg: track.album.images[1].url,
+                };
+              })
+            )
+          );
+          dispatch(playlistActions.setCurPlaylist(searchSongs));
+        }
       }}
     >
       <OutlinedInput
-      onKeyDown={(e) => {if (e.key === "Enter") {e.target.form.requestSubmit();}}}
+        onKeyDown={(e) => {
+          const target = e.target as HTMLInputElement;
+          if (e.key === 'Enter') {
+            target.form?.requestSubmit();
+          }
+        }}
         value={search}
         sx={{
           borderRadius: 10,
