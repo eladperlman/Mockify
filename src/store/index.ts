91526c4541aa5cbb4a15ff4854fbdb17abbd4ba3
Elad Perlman <ep3213@gmail.com>
2023-07-02 16:35:07 -0400

    fixed build warnings

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { playlistSlice } from './playlistSlice';
 import { searchSlice } from './searchSlice';
 import { apiSlice } from './api/apiSlice';
